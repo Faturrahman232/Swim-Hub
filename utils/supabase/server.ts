8db9c88d545d87f93166bf5ceca032c3f03c4cc9
Faturrahman232 <fatur020302@gmail.com>
2026-04-07 21:59:48 +0700

    fix: typing cookiesToSet server

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -12,17 +12,17 @@ export const createClient = async () => {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(
+          cookiesToSet: { name: string; value: string; options?: any }[]
+        ) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options);
             });
           } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            console.error("Error setting cookies:", error);
           }
-        },
+        }
       },
     },
   );
